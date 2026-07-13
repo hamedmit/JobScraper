0e9cb5cc95b45eeb5a237ca949bf38ddc4f5c498
Hamed <hamedmit.gh@gmail.com>
2026-07-13 11:36:17 +0330

    making logs

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello, GitHub Actions is working!")
+from src.logger import logger
+
+
+def main():
+    logger.info("=" * 50)
+    logger.info("JobScraper started.")
+    logger.info("Logger initialized successfully.")
+    logger.info("JobScraper finished.")
+    logger.info("=" * 50)
+
+
+if __name__ == "__main__":
+    main()
